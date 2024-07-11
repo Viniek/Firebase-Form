fc22de3dee05e8e74701ed7d9f38601ed30ad800
unknown <njerivictory52@gmail.com>
2024-07-11 16:28:47 +0300

    finalize authorization

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,6 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCB7PmQepIwaQUz_9H5SRRPBgYBk4O48jg",
   authDomain: "fir-app-2184b.firebaseapp.com",
   projectId: "fir-app-2184b",
+  
   storageBucket: "fir-app-2184b.appspot.com",
   messagingSenderId: "1026098737557",
   appId: "1:1026098737557:web:a08a3ff71dfe5a074cc69c",
